39ce8c30231c01ee293b368ccce52e656462d97a
andyWynyard <andrew.wynyard@gmail.com>
2017-04-09 11:06:18 +0200

    ex.41, didnt work when all text was on one line. Important to note.

--- GettingFromFile.java
+++ GettingFromFile.java
@@ -6,5 +6,17 @@ public class GettingFromFile {
 		String name;
 		int a, b, c, sum;
 		
+		System.out.print("Getting name and three numbers from file...");
+		Scanner fileIn = new Scanner(new File("name-and-numbers.txt"));
+			name = fileIn.nextLine();
+			a = fileIn.nextInt();
+			b = fileIn.nextInt();
+			c = fileIn.nextInt();
+		fileIn.close();
+
+		System.out.println("done");
+		System.out.println("Your name is " + name);
+		sum  = a + b + c;
+		System.out.println(a + "+" + b + "+" + c + "=" + sum);
 	}
 }
